257d46e696e5caba107710d8e77db7aaadebb8eb
infamy <alex@pixelfactor.ca>
2014-03-11 01:19:44 -0700

    initial commit.
    Simple proxy, needs work done to the handle response.

---
+++ binfrak.py
@@ -0,0 +1,54 @@
+from twisted.python import log
+from twisted.web import http, proxy
+
+class ProxyClient(proxy.ProxyClient):
+    """Mange returned header, content here.
+	Use `self.father` methods to modify request directly.
+    """
+    def handleHeader(self, key, value):
+        # change response header here
+        log.msg("Header: %s: %s" % (key, value))
+        proxy.ProxyClient.handleHeader(self, key, value)
+	
+    def handleResponse(self, data):
+        if (self.isCompressed):
+            logging.debug("Decompressing content...")
+            data = gzip.GzipFile('', 'rb', 9, StringIO.StringIO(data)).read()
+            
+        log.msg( "Read from server:\n" + data)
+
+        if (self.contentLength != None):
+            self.client.setHeader('Content-Length', len(data))
+        
+        self.client.write(data)
+        self.shutdown()
+
+class ProxyClientFactory(proxy.ProxyClientFactory):
+    protocol = ProxyClient
+
+class ProxyRequest(proxy.ProxyRequest):
+    protocols = dict(http=ProxyClientFactory)
+
+class Proxy(proxy.Proxy):
+    requestFactory = ProxyRequest
+
+class ProxyFactory(http.HTTPFactory):
+    protocol = Proxy
+	
+listenport = 10000
+
+if __name__ == '__main__':
+    import sys
+    from twisted.internet import endpoints, reactor
+
+    def shutdown(reason, reactor, stopping=[]):
+        """Stop the reactor."""
+        if stopping: return
+        stopping.append(True)
+        if reason:
+            log.msg(reason.value)
+        reactor.callWhenRunning(reactor.stop)
+
+    log.startLogging(sys.stdout)
+    reactor.listenTCP(int(listenport),ProxyFactory())
+    reactor.run()
